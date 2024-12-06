721295d9854b6f9c77343a6deaeb8d15ba7b9c8a
Michael Hirsch <michael@hirsch.me>
2024-12-05 19:33:09 -0600

    override ethers send transaction to create facet transactions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,25 +1,88 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
-import dotenv from 'dotenv';
+import { JsonRpcProvider, MaxUint256, Wallet } from "ethers";
+import { extendEnvironment, HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import { prepareFacetTransaction, viem as facetViem } from "@0xfacet/sdk";
 
-dotenv.config(); // Load environment variables from .env file
+const FACET_INBOX_ADDRESS =
+  "0x00000000000000000000000000000000000FacE7" as const;
+
+require('dotenv').config();
+
+// Step 1: Extend Hardhat's environment
+extendEnvironment((hre) => {
+  const originalGetSigners = hre.ethers.getSigners;
+
+  // Override the getSigners method to return custom signers
+  hre.ethers.getSigners = async () => {
+    const signers = await originalGetSigners();
+    return await Promise.all(signers.map(async (signer) => {
+      const l2Network = await signer.provider.getNetwork()
+      const l1Chain = {
+        url: l2Network.chainId === BigInt(0xface7) ? "https://ethereum-rpc.publicnode.com" : "https://ethereum-sepolia-rpc.publicnode.com",
+        chainId: l2Network.chainId === BigInt(0xface7) ? 1 : 11155111,
+        name: l2Network.chainId === BigInt(0xface7) ? "mainnet" : "sepolia"
+      }
+      const l1Provider = new JsonRpcProvider(l1Chain.url, { name: l1Chain.name, chainId: l1Chain.chainId })
+      const l1Signer = new Wallet(process.env.WALLET_KEY!, l1Provider)
+      console.log(l1Signer)
+      signer.sendTransaction = async (req) => {
+        const facetPublicClient = facetViem.createFacetPublicClient(l1Chain.chainId as 1 | 11155111);
+        
+        const [estimateGasRes, fctMintRate] = await Promise.all([
+          facetPublicClient.estimateGas({
+            account: l1Signer.address as `0x${string}`,
+            to: req.to as `0x${string}` | undefined,
+            value: req.value ? BigInt(req.value.toString()) : undefined as bigint | undefined,
+            data: req.data as `0x${string}` | undefined,
+            stateOverride: [
+              { address: l1Signer.address as `0x${string}`, balance: MaxUint256 },
+            ],
+          }),
+          facetViem.getFctMintRate(l1Chain.chainId as 1 | 11155111),
+        ]);
+
+        const gasLimit = estimateGasRes;
+              
+        const { encodedTransaction } = await prepareFacetTransaction(
+          Number(l2Network.chainId),
+          fctMintRate,
+          { to: req.to as `0x${string}` | undefined,
+            value: req.value ? BigInt(req.value.toString()) : undefined as bigint | undefined,
+            data: req.data as `0x${string}` | undefined, gasLimit
+          }
+        );
+
+        const l1Transaction = {
+          to: FACET_INBOX_ADDRESS,
+          value: 0n,
+          data: encodedTransaction,
+        };
+        
+        return l1Signer.sendTransaction(l1Transaction)
+      };
+      return signer
+    }));
+  };
+});
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.26",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+    version: '0.8.26',
   },
   networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY as string]
-    }
-  }
+    'facet-sepolia': {
+      chainId: 0xface7a,
+      url: 'https://sepolia.facet.org',
+      accounts: [process.env.WALLET_KEY as string],
+      // gasPrice: 1000000000,
+    },
+    'sepolia': {
+      chainId: 11155111,
+      url: 'https://ethereum-sepolia-rpc.publicnode.com',
+      accounts: [process.env.WALLET_KEY as string],
+    },
+  },
+  defaultNetwork: 'facet-sepolia',
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,109 +1,15 @@
-import { encodeFunctionData, decodeFunctionResult, formatEther } from 'viem';
-import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
-import { sendFacetTransaction } from './sendFacetTransaction';
-
-dotenv.config();
+import { ethers } from 'hardhat';
 
 async function main() {
-  const contractPath = path.resolve(__dirname, '../artifacts/contracts/SimpleStorage.sol/SimpleStorage.json');
-  const contractArtifact = JSON.parse(fs.readFileSync(contractPath, 'utf8'));
-  const bytecode = contractArtifact.bytecode;
-  const abi = contractArtifact.abi;
-  
-  const facetTransaction = await sendFacetTransaction({
-    maxFeePerGas: 10,
-    gasLimit: 500_000,
-    data: bytecode as `0x${string}`
-  })
+  const nft = await ethers.deployContract('NFT');
 
-  console.log("Facet Transaction:", facetTransaction);
-  
-  const contractAddress = facetTransaction.facet_transaction_receipt.contract_address;
-  console.log("Contract Address:", contractAddress);
+  await nft.waitForDeployment();
 
-  const setValueData = encodeFunctionData({
-    abi: abi,
-    functionName: "setValue",
-    args: [42]
-  });
-  
-  const facetTransaction2 = await sendFacetTransaction({
-    to: contractAddress as `0x${string}`,
-    maxFeePerGas: 0, // Magic value to use base fee
-    gasLimit: 500_000,
-    data: setValueData as `0x${string}`
-  })
-
-  console.log("Facet Transaction 2:", facetTransaction2);
-  
-  const txHash = facetTransaction2.tx_hash
-  console.log("Transaction hash:", txHash);
-  
-  const getValueData = encodeFunctionData({
-    abi: abi,
-    functionName: "getValue",
-    args: [],
-  });
-  
-  const rpcBaseUrl = 'https://testnet-alpha.facet.org/rpc'
-  
-  const ethCallPayload = {
-    jsonrpc: "2.0",
-    method: "eth_call",
-    params: [
-      {
-        to: contractAddress as `0x${string}`,
-        data: getValueData as `0x${string}`
-      },
-      "latest"
-    ],
-    id: 1
-  };
-
-  const response = await fetch(rpcBaseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(ethCallPayload)
-  });
-
-  const result = await response.json();
-  console.log("eth_call result:", result);
-
-  const decodedResult = decodeFunctionResult({
-    abi: abi,
-    functionName: "getValue",
-    data: result.result as `0x${string}`
-  });
-
-  console.log("Decoded result:", decodedResult);
-  
-  const balancePayload = {
-    jsonrpc: "2.0",
-    method: "eth_getBalance",
-    params: [
-      facetTransaction2.from_address as `0x${string}`,
-      "latest"
-    ],
-    id: 2
-  };
-
-  const balanceResponse = await fetch(rpcBaseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(balancePayload)
-  });
-
-  const balanceResult = await balanceResponse.json();
-  const balance = BigInt(balanceResult.result);
-  console.log("Deployer balance:", formatEther(balance));
+  console.log('NFT Contract Deployed at ' + nft.target);
 }
 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
